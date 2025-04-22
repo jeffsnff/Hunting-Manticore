d6bc82a1b67e3113e72cf009599832b15bff4b39
Jeffrey Seneff <jeffsnff@gmail.com>
2025-04-22 18:45:30 -0400

    Created Manticore class (on main) and created ChooseDistance method for player 1

--- Manticore/Program.cs
+++ Manticore/Program.cs
@@ -6,7 +6,26 @@ namespace Manticore
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World");
+
+      int distance = ChooseDistance();
+
+      Manticore manticore = new Manticore(distance);
+      Console.WriteLine(manticore);
+    }
+
+    public static int ChooseDistance()
+    {
+      while (true)
+      {
+        Console.Write("Player 1 choose a distance for the airship between 1 and 100: ");
+        int distance = Convert.ToInt32(Console.ReadLine());
+        if (distance > 100 || distance < 1)
+        {
+          Console.WriteLine("Invalid distance.");
+          continue;
+        }
+        return distance;
+      }
     }
   }
 }
