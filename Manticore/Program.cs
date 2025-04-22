59a17f3dc707b45b6512f1190aacc661037b16d2
Jeffrey Seneff <jeffsnff@gmail.com>
2025-04-22 19:42:26 -0400

    added XML comments, damage calculator, and RoundInformation display

--- Manticore/Program.cs
+++ Manticore/Program.cs
@@ -12,7 +12,7 @@ namespace Manticore
 
       while (true)
       {
-        Console.WriteLine($"Round: {round} City: {consolas} Manticore: {manticore}");
+        RoundInformation(manticore, consolas, round);
         GameRound(manticore, consolas, round);
         if (consolas.GetHealth() <= 0)
         {
@@ -30,6 +30,10 @@ namespace Manticore
       }
     }
 
+    /// <summary>
+    /// Ask user to choose a range to fire
+    /// </summary>
+    /// <returns>distance to calculate if a success</returns>
     public static int ChooseDistance()
     {
       while (true)
@@ -45,11 +49,40 @@ namespace Manticore
       }
     }
 
+    /// <summary>
+    /// One round of the game, calculating health of manticore, consolas and round
+    /// </summary>
+    /// <param name="manticore"></param>
+    /// <param name="consolas"></param>
+    /// <param name="round"></param>
     public static void GameRound(Manticore manticore, City consolas, int round)
     {
       Console.Write("Enter desired cannon range: ");
       manticore.updateHealth(round, Convert.ToInt32(Console.ReadLine()));
       consolas.UpdateHealth();
+    }
+
+    public static void RoundInformation(Manticore manticore, City consolas, int round)
+    {
+      Console.WriteLine($"Round: {round} City: {consolas} Manticore: {manticore}");
+      Console.WriteLine($"The cannon is expected to deal {Damage(round)} damage this round.");
+    }
+    /// <summary>
+    /// Calculates amount of damage to do
+    /// </summary>
+    /// <param name="round"></param>
+    /// <returns>Damage output</returns>
+    public static int Damage(int round)
+    {
+      if (round % 3 == 0 && round % 5 == 0)
+      {
+        return 10;
+      }
+      if (round % 3 == 0 || round % 5 == 0)
+      {
+        return 3;
+      }
+      return 1;
     }
   }
 }
